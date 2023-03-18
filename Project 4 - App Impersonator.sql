9e53a29d2982f188dc47b61c788d38230d3ab813
Javier Alan Castellanos <j.alan.castellanos@gmail.com>
2023-03-17 19:12:59 -0500

    Update Project 4 - App Impersonator.sql

--- Project 4 - App Impersonator.sql
+++ Project 4 - App Impersonator.sql
@@ -7,8 +7,7 @@ Use an UPDATE to emulate what happens when you edit data in the app.
 Use a DELETE to emulate what happens when you delete data in the app.
 */
 
-/* What does the app's SQL look like? */
-
+-- Create a video game database
 CREATE TABLE rocket_league (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     player TEXT,
@@ -17,24 +16,28 @@ CREATE TABLE rocket_league (
     losses INTEGER
 );
 
+-- Inserting video game instances
 INSERT INTO rocket_league (player, games_played, wins, losses)
-    VALUES ("JavierACM11", 5, 2, 3);
-    
+    VALUES ("JavierACM11", 5, 2, 3);    
 INSERT INTO rocket_league (player, games_played, wins, losses)
     VALUES ("JavierACM11", 6, 4, 2);
-    
 INSERT INTO rocket_league (player, games_played, wins, losses)
     VALUES ("JavierACM11", 1, 1, 0);
-    
+
+-- View the database
 SELECT * FROM rocket_league;
 
+-- Update the database
 UPDATE rocket_league
 SET wins = 0, losses = 1
 WHERE id = 3;
 
+-- View the updated database
 SELECT * FROM rocket_league;
 
+-- Delete a row from the database
 DELETE FROM rocket_league
 WHERE id = 2;
 
+-- View the final database
 SELECT * FROM rocket_league;
