ceb48c1a0e2a24fc92612d8e5703e0a02d309fd2
Javier Alan Castellanos <j.alan.castellanos@gmail.com>
2023-03-17 19:16:57 -0500

    Update Project 4 - App Impersonator.sql

--- Project 4 - App Impersonator.sql
+++ Project 4 - App Impersonator.sql
@@ -1,5 +1,7 @@
 /*
-Think about your favorite apps, and pick one that stores your data- like a game that stores scores, an app that lets you post updates, etc. Now in this project, you're going to imagine that the app stores your data in a SQL database (which is pretty likely!), and write SQL statements that might look like their own SQL.
+Think about your favorite apps, and pick one that stores your data- like a game that stores scores, an app that lets you post updates, etc. 
+Now in this project, you're going to imagine that the app stores your data in a SQL database (which is pretty likely!), and write SQL 
+statements that might look like their own SQL.
 
 CREATE a table to store the data.
 INSERT a few example rows in the table.
